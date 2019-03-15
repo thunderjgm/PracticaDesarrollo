b7c47c9a910dce3a818c743c1a0ffd1af6cc780c
thunderjgm <47524972+thunderjgm@users.noreply.github.com>
2019-03-15 01:43:06 -0500

    Add files via upload

---
+++ celularesdb.sql
@@ -0,0 +1,130 @@
+-- MySQL dump 10.16  Distrib 10.3.10-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: celularesdb
+-- ------------------------------------------------------
+-- Server version	10.3.10-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `detalles`
+--
+
+DROP TABLE IF EXISTS `detalles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detalles` (
+  `id_detalles` int(11) NOT NULL,
+  `RAM` int(11) NOT NULL,
+  `resolucion_camara` int(11) NOT NULL,
+  `capacidad_bateria` int(11) NOT NULL,
+  `capacidad_almacenamiento` int(11) NOT NULL,
+  `idreferencia` int(11) NOT NULL,
+  PRIMARY KEY (`id_detalles`),
+  KEY `FK_detalles` (`idreferencia`),
+  CONSTRAINT `FK_detalles` FOREIGN KEY (`idreferencia`) REFERENCES `referencia` (`idreferencia`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalles`
+--
+
+LOCK TABLES `detalles` WRITE;
+/*!40000 ALTER TABLE `detalles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detalles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login` (
+  `idlogin` int(11) NOT NULL,
+  `user` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`idlogin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (1,'root','root'),(2,'admin','admin');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marca`
+--
+
+DROP TABLE IF EXISTS `marca`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marca` (
+  `idmarca` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`idmarca`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marca`
+--
+
+LOCK TABLES `marca` WRITE;
+/*!40000 ALTER TABLE `marca` DISABLE KEYS */;
+/*!40000 ALTER TABLE `marca` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `referencia`
+--
+
+DROP TABLE IF EXISTS `referencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `referencia` (
+  `idreferencia` int(11) NOT NULL,
+  `nombreReferencia` varchar(45) NOT NULL,
+  `idmarca` int(11) NOT NULL,
+  PRIMARY KEY (`idreferencia`),
+  KEY `FK_referencia` (`idmarca`),
+  CONSTRAINT `FK_referencia` FOREIGN KEY (`idmarca`) REFERENCES `marca` (`idmarca`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `referencia`
+--
+
+LOCK TABLES `referencia` WRITE;
+/*!40000 ALTER TABLE `referencia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `referencia` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-14 12:24:59
